d503e4567b73408b8fba935a6948e4a593607851
shulin <shulin@uoko.com>
2018-12-03 16:07:46 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import './assets/style/reset.css'
+import './assets/style/border.css'
+import fastClick from 'fastclick'
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>Hello World</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style scoped>
+
+</style>
